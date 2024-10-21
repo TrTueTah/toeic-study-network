f318b7ba45ff50dc7f79dcba3aa89930e2f237cf
Tanh_Chan <119776880+TrTueTah@users.noreply.github.com>
2024-10-21 16:09:05 +0700

    init

---
+++ ToeicStudyNetwork/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ToeicStudyNetwork.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
