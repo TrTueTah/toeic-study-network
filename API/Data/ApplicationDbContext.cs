d3105e000339b3d594eafb989573cefc69da7c1a
Tanh_Chan <119776880+TrTueTah@users.noreply.github.com>
2024-10-23 16:39:19 +0700

    Connect to Database

---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+        
+    }
+    public DbSet<Person> Persons { get; set; }
+}
