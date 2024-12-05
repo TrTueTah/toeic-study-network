1436c1244ad78d23cd9639cb712270954a904a5e
HGB3009 <baobaokg9@gmail.com>
2024-12-05 14:43:53 +0700

    refactorGetUserResult api

--- API/Dtos/ResultDto/UserResultDto.cs
+++ API/Dtos/ResultDto/UserResultDto.cs
@@ -6,7 +6,13 @@ public class UserResultDto
     public string UserId { get; set; }
     public string ExamId { get; set; }
     public int Score { get; set; }
+    public int ReadingScore { get; set; }
+    public int ListeningScore { get; set; }
+    public int ReadingCorrectAnswerAmount { get; set; }
+    public int ListeningCorrectAnswerAmount { get; set; }
     public int CorrectAnswerAmount { get; set; }
+    public int IncorrectAnswerAmount { get; set; }
+    public int WithoutAnswerAmount { get; set; }
     public float TimeTaken { get; set; }
     public string Type { get; set; }
     public List<DetailResultDto> DetailResults { get; set; }
