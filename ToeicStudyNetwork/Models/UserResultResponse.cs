55ea6469b5f2e99799f044154e8f71a9542770c7
not-thanhtai <tai606760@gmail.com>
2024-12-05 16:09:50 +0700

    Add user image and username to ViewBag; enhance UserResultResponse model and update styles

--- ToeicStudyNetwork/Models/UserResultResponse.cs
+++ ToeicStudyNetwork/Models/UserResultResponse.cs
@@ -5,9 +5,25 @@ public class UserResultResponse
     public string? UserResultId { get; set; }
     public string? UserId { get; set; }
     public string? ExamId { get; set; }
+    public string? ExamName { get; set; }
     public int Score { get; set; }
+    public int ReadingScore { get; set; }
+    public int ListeningScore { get; set; }
+    public int ReadingCorrectAnswerAmount { get; set; }
+    public int ListeningCorrectAnswerAmount { get; set; }
     public int CorrectAnswerAmount { get; set; }
+    public int IncorrectAnswerAmount { get; set; }
+    public int WithoutAnswerAmount { get; set; }
     public float TimeTaken { get; set; }
+    
+    public string TimeTakenFormatted
+    {
+        get
+        {
+            TimeSpan time = TimeSpan.FromSeconds(TimeTaken);
+            return time.ToString(@"hh\:mm\:ss");
+        }
+    }
     public DateTime CreatedAt { get; set; }
     public string? Type { get; set; }
     public List<DetailResultModel> DetailResults { get; set; }
