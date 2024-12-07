09aa6865a13a4f38d4eca416a1b3f3b0f678a829
HGB3009 <baobaokg9@gmail.com>
2024-12-07 20:27:11 +0700

    Add TotalQuestions calculation to GetAllUserResultDto and update mapping

--- API/Dtos/ResultDto/GetAllUserResultDto.cs
+++ API/Dtos/ResultDto/GetAllUserResultDto.cs
@@ -5,6 +5,7 @@ public class GetAllUserResultDto
     public string UserResultId { get; set; }
     public string ExamId { get; set; }
     public string ExamName { get; set; }
+    public int TotalQuestions { get; set; }
     public int Score { get; set; }
     public int CorrectAnswerAmount { get; set; }
     public float TimeTaken { get; set; }
