c27956f96512198afd59b5ccc66463283470071a
not-thanhtai <tai606760@gmail.com>
2024-12-10 22:20:57 +0700

    Complete posts lazy loading, handle toggle like post, fetch liked posts for user, and split view into some components

--- ToeicStudyNetwork/Controllers/ForumController.cs
+++ ToeicStudyNetwork/Controllers/ForumController.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using ToeicStudyNetwork.Models;
 using Newtonsoft.Json;
-using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using ToeicStudyNetwork.Dtos;
 using ToeicStudyNetwork.ViewModels.Forum;
@@ -19,15 +17,11 @@ namespace ToeicStudyNetwork.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index(int pageIndex, int pageSize = 5)
+        public async Task<IActionResult> Index(int pageIndex = 0, int pageSize = 5)
         {
             var response = await FetchAllPosts(pageIndex + 1, pageSize);
-
-            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
-            {
-                return PartialView("_PostListPartial", response.Results);
-            }
-
+            var currentUserId = Request.Cookies["userId"];
+            
             var posts = response.Results.Select(p => new PostViewModel
             {
                 Id = p.Id,
@@ -38,20 +32,61 @@ namespace ToeicStudyNetwork.Controllers
                 Comments = p.Comments,
                 UserId = p.UserId,
                 UserName = p.UserName,
-                UserImageUrl = p.UserImageUrl
+                UserImageUrl = p.UserImageUrl,
+                IsLike = p.Likes.Any(like => like.UserId == currentUserId)
             }).ToList();
-
+            
             ViewBag.UserImageUrl = Request.Cookies["userImage"];
             ViewBag.UserName = Request.Cookies["given_name"];
+            ViewBag.CurrentTab = "newest";
+            
+            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                return PartialView("_PostListPartial", posts);
+            }
+            
             return View(posts);
         }
+        
+        [HttpGet("liked")]
+        public async Task<IActionResult> Liked(int pageIndex = 0, int pageSize = 5)
+        {
+            var response = await FetchLikedPostsByUserId(pageIndex + 1, pageSize);
+            
+            var currentUserId = Request.Cookies["userId"];
+            
+            var posts = response.Results.Select(p => new PostViewModel
+            {
+                Id = p.Id,
+                Content = p.Content,
+                MediaUrls = p.MediaUrls,
+                CreatedAt = p.CreatedAt,
+                Likes = p.Likes,
+                Comments = p.Comments,
+                UserId = p.UserId,
+                UserName = p.UserName,
+                UserImageUrl = p.UserImageUrl,
+                IsLike = p.Likes.Any(like => like.UserId == currentUserId)
+            }).ToList();
+            
+            ViewBag.UserImageUrl = Request.Cookies["userImage"];
+            ViewBag.UserName = Request.Cookies["given_name"];
+            ViewBag.CurrentTab = "liked";
+
+            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                return PartialView("_PostListPartial", posts);
+            }
+            
+            return View(posts);
+        }
+
         
         [HttpGet("post/{postId}")]
         public async Task<IActionResult> PostDetail([FromRoute] string postId)
         {
             var postData = await FetchPostById(postId);
             
-           
             ViewBag.UserImageUrl = Request.Cookies["userImage"];
             ViewBag.UserName = Request.Cookies["given_name"];
             
@@ -135,43 +170,47 @@ namespace ToeicStudyNetwork.Controllers
         [HttpPost("ToggleLike")]
         public async Task<IActionResult> ToggleLike([FromBody] ToggleLikeRequest request)
         {
-            if (request == null || string.IsNullOrEmpty(request.PostId))
+            if (string.IsNullOrEmpty(request.PostId))
             {
                 return BadRequest("Invalid PostId");
             }
 
-            var token = Request.Cookies["token"];
-            if (string.IsNullOrEmpty(token))
-            {
-                return RedirectToAction("Login", "Account");
-            }
-
-            var handler = new JwtSecurityTokenHandler();
-            var jwtToken = handler.ReadToken(token) as JwtSecurityToken;
-
-            var userEmail = Request.Cookies["email"];
-            var userIdResponse = await _httpClient.GetAsync($"http://localhost:5112/api/v1/users/getUserIdByEmail/{userEmail}");
-            userIdResponse.EnsureSuccessStatusCode();
-            var userId = await userIdResponse.Content.ReadAsStringAsync();
+          
+            var userId = Request.Cookies["userId"];
 
             var likeData = new { UserId = userId, PostId = request.PostId };
 
             var response = await _httpClient.PostAsync("http://localhost:5112/api/v1/like/toggleLike",
                 new StringContent(JsonConvert.SerializeObject(likeData), Encoding.UTF8, "application/json"));
-
+            
             if (!response.IsSuccessStatusCode)
             {
                 var errorMessage = await response.Content.ReadAsStringAsync();
                 return BadRequest($"Error: {errorMessage}");
             }
-
-            return Ok(new { Success = true });
+            
+            var responseData = JsonConvert.DeserializeObject<ToggleLikeResponse>(await response.Content.ReadAsStringAsync());
+            
+            return Ok(new { Success = true, IsLiked = responseData.IsLiked });
         }
         
         [NonAction]
         private async Task<PostResponse> FetchAllPosts(int pageIndex, int pageSize)
         {
             var response = await _httpClient.GetAsync($"http://localhost:5112/api/v1/post/getAllPosts?page={pageIndex}&limit={pageSize}");
+            response.EnsureSuccessStatusCode();
+
+            var responseData = await response.Content.ReadAsStringAsync();
+            var posts = JsonConvert.DeserializeObject<PostResponse>(responseData);
+
+            return posts;
+        }
+        
+        [NonAction]
+        private async Task<PostResponse> FetchLikedPostsByUserId(int pageIndex, int pageSize)
+        {
+            var userId = Request.Cookies["userId"];
+            var response = await _httpClient.GetAsync($"http://localhost:5112/api/v1/post/getLikedPostsByUserId/{userId}?page={pageIndex}&limit={pageSize}");
             response.EnsureSuccessStatusCode();
 
             var responseData = await response.Content.ReadAsStringAsync();
@@ -189,6 +228,7 @@ namespace ToeicStudyNetwork.Controllers
 
             var responseData = await response.Content.ReadAsStringAsync();
             var post = JsonConvert.DeserializeObject<PostViewModel>(responseData);
+            
             return post;
         }
     }
