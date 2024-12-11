182c32cd355ad1d400dfd126f30aad7102404cee
not-thanhtai <tai606760@gmail.com>
2024-12-11 19:37:54 +0700

    Implement tab switching functionality and enhance user interface in profile and test detail views

--- ToeicStudyNetwork/wwwroot/js/profile_index.js
+++ ToeicStudyNetwork/wwwroot/js/profile_index.js
@@ -103,3 +103,28 @@ changeImageButton.addEventListener("click", () => {
       }
     });
 });
+
+function practiceTabActive () {
+  const practiceTabButton = document.getElementById("practice-tab-button");
+  const fullTestTabButton = document.getElementById("full-test-tab-button");
+  const practiceTab = document.getElementById("practice-tab");
+  const fullTestTab = document.getElementById("full-test-tab");
+
+  practiceTabButton.classList.add("active");
+  fullTestTabButton.classList.remove("active");
+  practiceTab.classList.remove("d-none");
+  fullTestTab.classList.add("d-none");
+}
+
+function fullTestTabActive () {
+  const practiceTabButton = document.getElementById("practice-tab-button");
+  const fullTestTabButton = document.getElementById("full-test-tab-button");
+  const practiceTab = document.getElementById("practice-tab");
+  const fullTestTab = document.getElementById("full-test-tab");
+
+  practiceTabButton.classList.remove("active");
+  fullTestTabButton.classList.add("active");
+  practiceTab.classList.add("d-none");
+  fullTestTab.classList.remove("d-none");
+}
+
